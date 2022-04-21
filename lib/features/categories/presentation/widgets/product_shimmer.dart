dffb415ab61420606e8f37203e669281b0d39521
taha <tahaelkholy.dev@gmail.com>
2022-04-21 15:31:03 +0200

    add shimmer in category

--- lib/features/categories/presentation/widgets/product_shimmer.dart
+++ lib/features/categories/presentation/widgets/product_shimmer.dart
@@ -9,8 +9,9 @@ class ProductShimmer extends StatelessWidget {
     return GridView.count(
       padding: const EdgeInsets.all(10),
       crossAxisCount: 2,
+      crossAxisSpacing: 10,
       childAspectRatio: .6,
-      //physics: const NeverScrollableScrollPhysics(),
+      physics: const NeverScrollableScrollPhysics(),
       children: List.generate(20, (index) => const ShimmerItem()),
     );
   }
@@ -24,46 +25,61 @@ class ShimmerItem extends StatelessWidget {
     return Shimmer.fromColors(
       baseColor: Colors.grey.shade500,
       highlightColor: Colors.grey.shade300,
-      child: GridTile(
-        child: Column(
-          children: [
-            AspectRatio(
-              aspectRatio: 1,
-              child: Container(
-                width: double.infinity ,
-                decoration: BoxDecoration(
-                    color: Colors.grey.shade500,
-                    borderRadius: BorderRadius.circular(24)),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          AspectRatio(
+            aspectRatio: 1,
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.grey.shade500,
+                  borderRadius: BorderRadius.circular(24)),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Padding(
+            padding: EdgeInsetsDirectional.only(end: 30),
+            child: ShimmerWidget(),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: const [
+              ShimmerWidget(
+                width: 100,
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Padding(
-              padding: EdgeInsetsDirectional.only(end: 30),
-              child: ShimmerWidget(),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Padding(
-              padding: EdgeInsetsDirectional.only(end: 30),
-              child: ShimmerWidget(),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              children: const [
-                ShimmerWidget(),
-                SizedBox(width: 20),
-                ShimmerWidget(
-                  width: 30,
-                ),
-              ],
-            ),
-          ],
-        ),
+              Spacer(),
+              ShimmerWidget(
+                width: 30,
+              ),
+              SizedBox(
+                width: 20,
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: const [
+              ShimmerWidget(
+                width: 80,
+                height: 10,
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              ShimmerWidget(
+                width: 80,
+                height: 10,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
